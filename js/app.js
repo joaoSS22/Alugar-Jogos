c399a1855db7c71fe45f19b85de879a239fdb4a8
Joao vitor <joaosoares07122004@gmail.com>
2024-10-01 17:38:35 -0300

    fix: Remodelando codigo para um formato mais legivel

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+        
+            imagem.classList.remove('dashboard__item__img--rented');
+    }else if(imagem.classList.contains('dashboard__item__img')){
+        
+        imagem.classList.add('dashboard__item__img--rented');
 
 
-function alterarStatus(button) {
+    }
 
-    if (button.classList.contains('dashboard__item__button--return')) 
-        {
-         button.classList.remove('dashboard__item__button--return'); 
-         button.textContent = 'alugar'; 
-
-    }else
-        {
-            button.classList.add('dashboard__item__button--return');
-            button.textContent = 'Devolver';
-        }
+    if (botao.classList.contains('dashboard__item__button--return')) {
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent ='Alugar';
+    }else{
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
 }
-
-
